103737eac9b9471971754c7126bbd7f5d3359745
temp12312yeap <temp12312@naver.com>
2024-01-31 11:39:12 +0900

    hello count and reverse

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref, onMounted} from 'vue'
+const count = ref({
+cnt: 0,
+name: "KIM",
+})
+
+function increment() {
+count.value.cnt += 2;
+count.value.name = count.value.name.split('').reverse().join('')
+}
+
+onMounted(() => console.log(`The initial count is ${count.value}.`)
+)
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+	 <button @click="increment">Count is : {{count.cnt }} {{count.name}} </button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+button {
+font-weight: bold;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
