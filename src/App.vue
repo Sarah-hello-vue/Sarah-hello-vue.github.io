fc918b7631f669b3ca67803d76945c8084ea5571
temp12312yeap <temp12312@naver.com>
2024-02-01 17:53:58 +0900

    ./Components/Child.vue module처럼 App.vue에서 사용하기

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,11 @@
 <script setup>
-import {ref } from 'vue'
+import {ref} from 'vue'
+import Child from './components/Child.vue'
 
-let id = 0
-const newTodo = ref('')
-const todos = ref([
- {id: id++, issue: 'Backlog'}, {id: id++, issue: 'Todo' },
- {id: id++, issue: 'Going Hawaii'}
-])
-                              
-function addTodo() {
- todos.value.push({id: id++,
- issue: newTodo.value});
- newTodo.value = '';
-}
-function removeTodo(item) {
-todos.value = todos.value.filter(todo => todo.id !== item.id) 
-}
+const pocketMoney = ref('당신에게 퇴근을 선물할께요...<📧>!')
+
 </script>
+
 <template>
-<form @submit.prevent="addTodo">
- <input v-model="newTodo">
-  <button> add  </button>
-</form>
-<ul>
- <li v-for = "todo in todos">
-  {{todo.issue}} - <button @click = "removeTodo(todo)"> remove  </button>
- </li>
-</ul>
+<Child :pocketMoney = "pocketMoney" />
 </template>
-
-
-
-
-
-
-
-
-
-
