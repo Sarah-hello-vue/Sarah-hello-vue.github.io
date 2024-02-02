00e26f48bc60fee5e792290f5fe85359fdf0df98
temp12312yeap <temp12312@naver.com>
2024-02-02 12:42:59 +0900

    동기화 및 fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <script setup>
-import {ref} from 'vue'
-let id = 4
-const show = ref(true)
-const list = ref([1,2,3])
+import {ref, watch} from 'vue'
+
+const todoId = ref(1);
+const todoData = ref(null)
+
+async function fetchData () { 
+
+ todoData.value = null
+ const res = await fetch (
+ `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+)
+todoData.value  = await res.json() 
+}
+
+//watch(() => todoId.value, fetchData, { immediate: true });
+fetchData()
+function get () {
+todoId.value++
+}
+
+watch(todoId, fetchData)
 </script>
+
 <template>
-<button @click ="show = !show"> List rendering on/off </button>
-<button @click="list.push(id++)"> add </button>
-<button @click="list.pop()"> remove </button>
-<button @click = "list.reverse()"> reverse </button>
-
-<ul v-if="show && list.length">
-	<li v-for= "item of list"> {{item}} </li>
-</ul>
-<p v-else-if="!show && list.length"> List is not empty, but hidden </p>
-<p v-else> List is empty</p>
-
+<p> Todo id: {{todoId}} </p>
+<button  @click ="get()"  :disabled="!todoData">Fetch next todo </button>
+<p v-if="!todoData"> Loading... </p>
+<pre v-else > {{todoData }} </pre>
 </template>
+
+
+
+
+
+
+
+
+
+
+
